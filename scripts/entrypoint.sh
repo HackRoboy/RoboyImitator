6ad25bc2790f96024230fbc8513dc0ede337b95a
yuriyarabskyy <yuriy.arabskyy@gmail.com>
2019-05-05 12:55:19 +0200

    change entrypoint code to python3

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tmux new-session -d -s recognition_node "python roboy_imitator/speech_to_text/recognition_node.py"
-tmux new-session -d -s main_loop "python roboy_imitator/main.py"
+tmux new-session -d -s recognition_node "python3 roboy_imitator/speech_to_text/recognition_node.py"
+tmux new-session -d -s main_loop "python3 roboy_imitator/main.py"
 tmux
